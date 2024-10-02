c54c485aaecedc80633e754e9819329a8f671378
revokr <101871749+revokr@users.noreply.github.com>
2024-10-02 12:48:52 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "YAML"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	warnings "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+    "src/**.cpp",
+
+    "include/**.h"
+	}
+
+  includedirs {
+    "include"
+  }
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
